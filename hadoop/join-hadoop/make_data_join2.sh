5e432c4af0743becef14905aedee11552f79b9d2
Martynas Asipauskas <me@martynas.co.uk>
2015-12-13 17:05:18 +0000

    Added initial setup for Simple Joins in Hadoop

---
+++ hadoop/join-hadoop/make_data_join2.sh
@@ -0,0 +1,6 @@
+python make_join2data.py y 1000 13 > data/join2_gennumA.txt
+python make_join2data.py y 2000 17 > data/join2_gennumB.txt
+python make_join2data.py y 3000 19 > data/join2_gennumC.txt
+python make_join2data.py n 100  23 > data/join2_genchanA.txt
+python make_join2data.py n 200  19 > data/join2_genchanB.txt
+python make_join2data.py n 300  37 > data/join2_genchanC.txt
